c26632aa8d33a7adda54fa86f030bfe8c6cf4501
Medaka <11Draghost11@gmail.com>
2026-03-16 09:45:17 +0200

    some fixes

--- app/main.py
+++ app/main.py
@@ -1,8 +1,9 @@
+from app.players.player import Player
 from app.players.elves.elf import Elf
 from app.players.dwarves.dwarf import Dwarf
 
 
-def calculate_team_total_rating(players: list) -> int | float:
+def calculate_team_total_rating(players: list[Player]) -> int | float:
     return sum(player.get_rating() for player in players)
 
 
